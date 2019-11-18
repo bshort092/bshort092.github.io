6886a6ebeeca31de495d25fc25b74444b227b1dc
Brandon Short <bshort092@gmail.com>
2019-11-18 11:38:33 -0700

    data is loaded and merged

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+d3.csv("data/tmdb_5000_movies.csv").then(movieInfo => {
+  let movies = [];
+  let credits = [];
+  movies = movieInfo.slice();
+  movieInfo.forEach(movie => {
+    //these were all stringified JSON arrays. Parsed them into js object arrays
+    movie.genres = JSON.parse(movie.genres)
+    movie.keywords = JSON.parse(movie.keywords)
+    movie.production_companies = JSON.parse(movie.production_companies)
+    movie.production_countries = JSON.parse(movie.production_countries)
+    movie.spoken_languages = JSON.parse(movie.spoken_languages)
+  })
+  d3.csv("data/tmdb_5000_credits.csv").then(creditInfo => {
+    credits = creditInfo.slice();
+    creditInfo.forEach(credit => {
+      //these were stringified JSON arrays. Parsed them into js object arrays
+      credit.cast = JSON.parse(credit.cast)
+      credit.crew = JSON.parse(credit.crew)
+    })
+    //sorting so indices line up
+    movies.sort((a, b) => (a.id > b.id) ? 1 : -1)
+    credits.sort((a, b) => (a.movie_id > b.movie_id) ? 1 : -1)
+
+    for(let i = 0; i < movies.length; i++){
+      if(movies[i].id === credits[i].movie_id) {
+        movies[i].cast = credits[i].cast
+        movies[i].crew = credits[i].crew
+      }
+      else {
+        console.log(`ERROR merging csv files: ${id} != ${movie_id}`)
+      }
+    }
+    console.log(movies)
+  });
+});
+
