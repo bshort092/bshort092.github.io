d5f953fd153dda08ec99d5e3d43dc2145b6ca831
Aaron Lafitte <aaron.lafitte@aggiemail.usu.edu>
2019-11-21 15:34:57 -0700

    Put a bar in place. Go me

---
+++ public/js/searchBar.js
@@ -0,0 +1,13 @@
+// Adding the search bar with all of its attributes
+
+
+d3.selectAll("#search-bar-container")
+  .append('input')
+  .attr('id', 'search-bar')
+  .attr('type', 'text')
+  .attr('spellcheck', 'false')
+  .attr('autocomplete', 'off')
+  .attr('autocapitalize', 'off')
+  .attr('display', 'block')
+  .attr('width', '100%')
+  ;
